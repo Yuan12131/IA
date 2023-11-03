9cab68adb851c94b300c4ae8e637193b72b1c80e
leeyuan <dbdks32123@gmail.com>
2023-11-03 14:20:50 +0900

    / URL GET req routing handler

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const port = 8080;
 // 정적 파일 요청 -> 미들웨어 함수
 app.use(express.static('public'));
 
+import { handleRootRequest } from "../mod/routes.mjs";
+// 루트 경로 GET 요청 index.html 파일 전송
+app.get('/', handleRootRequest);
+
 app.listen(port, function () {
   console.log(`http://127.0.0.1:${port}`);
 });
