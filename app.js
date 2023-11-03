c4a696e9629c5e569c5fb8144ceb19ee58543548
leeyuan <dbdks32123@gmail.com>
2023-11-03 14:37:44 +0900

    module CJS 방식으로 변경

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
-import express from 'express'; // ESM 방식으로 express 불러오기
-
+const express = require('express');
 const app = express();
+const { handleRootRequest } = require('./mod/routes'); // 라우팅 핸들러 모듈 가져오기
 
-// 포트번호
 const port = 8080;
 
-// 정적 파일 요청 -> 미들웨어 함수
+// 정적 파일을 불러오기 위한 미들웨어 설정
 app.use(express.static('public'));
 
-import { handleRootRequest } from "../mod/routes.mjs";
-// 루트 경로 GET 요청 index.html 파일 전송
-app.get('/', handleRootRequest);
 
 app.listen(port, function () {
   console.log(`http://127.0.0.1:${port}`);
