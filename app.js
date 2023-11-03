f5eb3358004e6901b69cb44a2b5b53bb7f10b988
leeyuan <dbdks32123@gmail.com>
2023-11-03 12:58:25 +0900

    static file - middleware func

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-import express from 'express';
+import express from 'express'; // ESM 방식으로 express 불러오기
 
 const app = express();
 
+// 포트번호
 const port = 8080;
+
+// 정적 파일 요청 -> 미들웨어 함수
+app.use(express.static('public'));
