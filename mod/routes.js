853211cee360dd4b64c57ae61c7d6fd7a463b3ce
leeyuan <dbdks32123@gmail.com>
2023-11-03 14:57:15 +0900

    dataRequest handler export

--- mod/routes.js
+++ mod/routes.js
@@ -1,9 +1,27 @@
-const express = require('express');
+const express = require("express");
+const http = require('http'); 
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.sendFile(__dirname + '../public/index.html')// 루트 경로 처리 로직
+
+router.get("/", (req, res) => {
+  res.sendFile(__dirname + "../public/index.html"); // 루트 경로 처리 로직
 });
 
-module.exports = { handleRootRequest: router.get('/') };
+router.post("/data", (req, res) => {
+
+  // http.get("/data", options, res => {
+    let data = '';
+
+    res.on('data', chunk => {
+      data += chunk;
+    });
+
+    res.on('end', () => {
+      res.writeHead(200, { 'Content-Type': 'text/json;charset=utf-8' });
+      res.end(data);
+    });
+  // });
+});
+
+module.exports = { handleRootRequest: router.get("/"), handleDataRequest: router.post("/data") };
