f60605db5335bb56dbadb1f81e8a6a2a510ac342
leeyuan <dbdks32123@gmail.com>
2023-11-05 17:39:29 +0900

    message POST 요청으로 data 전달 함수 수정

--- public/index.js
+++ public/index.js
@@ -9,50 +9,21 @@ searchButton.addEventListener('click', function () {
 });
 
 function data() {
-  const query = searchInput.value;
-  if (query) {
-  fetch('/send', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),
-  })
+  const message = searchInput.value;
+
+  if (message) {
+    fetch('/send', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ message }),
+    })
     .then((response) => response.json())
     .then((data) => {
       searchInput.value = '';
       displayResults(data);
     });
+  }
 }
-}
 
-// fetch("./data.json")//json파일 읽어오기
-//   .then((response) => response.json())
-//   .then((json) => {
-//       data = json.items;
-
-//       let html = '';
-//       data.forEach(element => {
-//           console.log(element)
-//       html+=`<li>
-//                   ${element.message}
-//               </li>`
-    
-//       });
-//       $('.list1').html(html)//화면에 출력
-//   });
-
-// 검색 결과를 화면에 표시하는 함수
-// function displayResults(data) {
-//   resultsDiv.innerHTML = ''; 
-
-//   if (data.items) {
-//     data.items.forEach(item => {
-//       const newRecoding = document.createElement('li');
-//       newRecoding.innerHTML = `<a href="${item.link}" target="_blank">${item.type}</a><p>${item.message}</p>`;
-//       resultsDiv.appendChild(newRecoding); // 결과 영역에 추가
-//     });
-//   } else {
-//     resultsDiv.innerHTML = '검색 결과가 없습니다.';
-//   }
-// }
