ab63bb1f02519880b70e0fbd4d083fbdfe84e0bb
leeyuan <dbdks32123@gmail.com>
2023-11-06 16:03:35 +0900

    주석 정리

--- mod/routes.js
+++ mod/routes.js
@@ -5,31 +5,32 @@ const fs = require("fs"); // fs 불러오기
 const router = express.Router();
 
 // JSON 파일 경로
-const jsonDataFilePath = 'data.json';
-const styleDataFilePath = 'style.json';
+const jsonDataFilePath = "data.json";
+const styleDataFilePath = "style.json";
 
-
-
+// 루트 경로 index.html 가져오기
 router.get("/", (req, res) => {
-  res.sendFile(__dirname + "../public/index.html"); // 루트 경로 처리 로직
+  res.sendFile(__dirname + "../public/index.html");
 });
 
-router.get('/json-data', (req, res) => {
+// JSON 경로 data.json 가져오기
+router.get("/json-data", (req, res) => {
   // data.json 파일 읽기
   fs.readFile(jsonDataFilePath, (err, data) => {
     if (err) {
-      res.status(500).send('Internal Server Error');
+      res.status(500).send("Internal Server Error");
     } else {
       res.json(JSON.parse(data));
     }
   });
 });
 
-router.get('/json-style', (req, res) => {
+// JSON 경로 style.json 가져오기
+router.get("/json-style", (req, res) => {
   // style.json 파일 읽기
   fs.readFile(styleDataFilePath, (err, data) => {
     if (err) {
-      res.status(500).send('Internal Server Error');
+      res.status(500).send("Internal Server Error");
     } else {
       res.json(JSON.parse(data));
     }
@@ -67,17 +68,18 @@ router.post("/send", (req, res) => {
         timestamp: timestamp,
       });
 
-      // 업데이트된 데이터를 JSON 파일에 다시 쓰기
-      // JavaScript 객체를 JSON 문자열로 변환하는 메서드인 JSON.stringify()를 사용
-      // jsonData: JSON으로 변환할 JavaScript 객체
-      // null : JSON 문자열을 들여쓰기 할 때 사용되는 공백 문자나 문자열
-      // 2 : JSON 문자열 내에서 들여쓰기를 표시할 때 사용할 공백 문자 수
+      /* 
+          업데이트된 데이터를 JSON 파일에 다시 쓰기
+          JavaScript 객체를 JSON 문자열로 변환하는 메서드인 JSON.stringify()를 사용
+          - jsonData: JSON으로 변환할 JavaScript 객체
+          - null : JSON 문자열을 들여쓰기 할 때 사용되는 공백 문자나 문자열
+          - 2 : JSON 문자열 내에서 들여쓰기를 표시할 때 사용할 공백 문자 수
+          */
       fs.writeFile("data.json", JSON.stringify(jsonData, null, 2), (err) => {
         if (err) {
           console.error("Error writing data.json:", err);
           res.status(500).send("Internal Server Error");
         } else {
-          // 클라이언트에 성공적인 응답 보내기
           res.json({
             status: "success",
             userMessage: message,
@@ -96,4 +98,9 @@ function generateAIResponse() {
   return responses[randomIndex];
 }
 
-module.exports = { handleRootRequest: router.get("/"), handleDataRequest: router.post("/send"), handleJsonStyleRequest:router.get("/json-style"), handleJsonDataRequest:router.get("/json-data") };
+module.exports = {
+  handleRootRequest: router.get("/"),
+  handleDataRequest: router.post("/send"),
+  handleJsonStyleRequest: router.get("/json-style"),
+  handleJsonDataRequest: router.get("/json-data"),
+};
