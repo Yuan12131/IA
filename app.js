c23028e1fbe00ce2ccc712b7ef842191b0b81616
leeyuan <dbdks32123@gmail.com>
2023-11-06 16:03:43 +0900

    주석 정리

--- app.js
+++ app.js
@@ -1,24 +1,31 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const { handleRootRequest, handleDataRequest, handleJsonStyleRequest, handleJsonDataRequest } = require('./mod/routes'); // 라우팅 핸들러 모듈 가져오기
+const {
+  handleRootRequest,
+  handleDataRequest,
+  handleJsonStyleRequest,
+  handleJsonDataRequest,
+} = require("./mod/routes"); // 라우팅 핸들러 모듈 가져오기
 
 const port = 8080;
 
 // 정적 파일을 불러오기 위한 미들웨어 설정
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // JSON 파싱을 위한 미들웨어 설정
-app.use(express.json()); 
+app.use(express.json());
 
 // 루트 경로로 접근했을 때 index.html 파일 전송
-app.get('/', handleRootRequest); // 라우팅 핸들러로 변경
+app.get("/", handleRootRequest);
 
-app.post('/send', handleDataRequest); // 라우팅 핸들러로 변경
+// POST 요청으로 메세지 전달
+app.post("/send", handleDataRequest);
 
-app.get('/json-data', handleJsonDataRequest);
+// data.json 파일 전송
+app.get("/json-data", handleJsonDataRequest);
 
-app.get('/json-style', handleJsonStyleRequest);
-
+//style.json 파일 전송
+app.get("/json-style", handleJsonStyleRequest);
 
 app.listen(port, function () {
   console.log(`http://127.0.0.1:${port}`);
